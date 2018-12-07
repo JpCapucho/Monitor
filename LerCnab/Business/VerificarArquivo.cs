2358401f04bcf8c358a7a31ddeec4061df056ed4
JpCapucho <jp.capucho@hotmail.com>
2018-12-06 23:46:34 -0200

    Ler Arquivo Cnab

---
+++ LerCnab/Business/VerificarArquivo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LerCnab.Business
+{
+    public class VerificarArquivo
+    {
+        /// <summary>
+        /// Se retornar um codigo válido, grava o arquivo em uma pasta para ser lido
+        /// </summary>
+        public void GravarArquivo()
+        {
+
+        }
+
+        /// <summary>
+        /// Ler o arquivo e retorna o Codigo do Banco, se existir!
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <returns><see cref="int CodigoBanco"/></returns>
+        public int CodigoBanco(Stream stream)
+        {
+            using (var reader = new StreamReader(stream))
+            {
+                try
+                {
+                    var file = reader.ReadLine();
+                    var codigo = Convert.ToInt32(file.Substring(76, 3));
+                    return codigo;
+                }
+                catch 
+                {
+                    throw new Exception("Erro ao ler o arquivo!");
+                }
+
+            }
+        }
+    }
+}
